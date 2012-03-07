8d2dd52931323c8b0e00bb5b3fb5e7bbdd30ba07
Matthias Krebs <makrebs@student.ethz.ch>
2012-03-07 15:16:08 +0100

    Fix minor bugs

--- src/uas/SkyeMAV.h
+++ src/uas/SkyeMAV.h
@@ -17,7 +17,7 @@ public slots:
     /** @brief Send the 6 DOF command (from 3d Mouse) to MAV */
     void setManualControlCommandsByMouse(double x , double y , double z , double a , double b , double c );
     /** @brief Send the 8 DOF command (from Testphase Widget) to MAV */
-    void setTestphaseCommandsByWidget(int Thrust1 , int Thrust3 , int Thrust3 , int Thrust4 , int Orientation1 , int Orientation2, int Orientation3, int Orientation4 ); //AL (06.03.12)
+    void setTestphaseCommandsByWidget(int Thrust1 , int Thrust2 , int Thrust3 , int Thrust4 , int Orientation1 , int Orientation2, int Orientation3, int Orientation4 ); //AL (06.03.12)
 
 protected:
     /** @brief Send a Direct Control Command to MAV: */
@@ -25,7 +25,7 @@ protected:
     /** @brief Send a Assisted Control Command to MAV: */
     void sendAssistedControlCommands(double xVel, double yVel, double zVel, double xRot, double yRot, double zRot);
     /** @brief Send a Testphase Control Command to MAV: */
-    void sendTestphaseControlCommands(int Thrust1 , int Thrust3 , int Thrust3 , int Thrust4 , int Orientation1 , int Orientation2, int Orientation3, int Orientation4 );//AL (06.03.12)
+    void sendTestphaseControlCommands(int Thrust1 , int Thrust2 , int Thrust3 , int Thrust4 , int Orientation1 , int Orientation2, int Orientation3, int Orientation4 );//AL (06.03.12)
 
 
     int manual1Thrust;          ///<Thrust for Motor 1 set by human pilot
