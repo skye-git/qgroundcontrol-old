f0cc662c6e2dc84a50d513bedea25bafab38d4b5
skye <skye.focuproject@gmail.com>
2012-04-02 22:09:57 +0200

    Changed Controller_Output message to float because Direct_Control is so

--- thirdParty/mavlink/include/skye/mavlink_msg_skye_controller_output.h
+++ thirdParty/mavlink/include/skye/mavlink_msg_skye_controller_output.h
@@ -5,16 +5,16 @@
 typedef struct __mavlink_skye_controller_output_t
 {
  uint32_t time_usec; ///< Timestamp (since UNIX epoch or microseconds since system boot)
- int16_t force_x; ///< Signal given from controller to actuation chain, 0.01 Newton
- int16_t force_y; ///< Signal given from controller to actuation chain, 0.01 Newton
- int16_t force_z; ///< Signal given from controller to actuation chain, 0.01 Newton
- int16_t moment_x; ///< Signal given from controller to actuation chain, 0.01 Newtonmeter
- int16_t moment_y; ///< Signal given from controller to actuation chain, 0.01 Newtonmeter
- int16_t moment_z; ///< Signal given from controller to actuation chain, 0.01 Newtonmeter
+ float force_x; ///< Signal given from controller to actuation chain, 1 Newton
+ float force_y; ///< Signal given from controller to actuation chain, 1 Newton
+ float force_z; ///< Signal given from controller to actuation chain, 1 Newton
+ float moment_x; ///< Signal given from controller to actuation chain, 1 Newtonmeter
+ float moment_y; ///< Signal given from controller to actuation chain, 1 Newtonmeter
+ float moment_z; ///< Signal given from controller to actuation chain, 1 Newtonmeter
 } mavlink_skye_controller_output_t;
 
-#define MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT_LEN 16
-#define MAVLINK_MSG_ID_160_LEN 16
+#define MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT_LEN 28
+#define MAVLINK_MSG_ID_160_LEN 28
 
 
 
@@ -22,12 +22,12 @@ typedef struct __mavlink_skye_controller_output_t
 	"SKYE_CONTROLLER_OUTPUT", \
 	7, \
 	{  { "time_usec", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_skye_controller_output_t, time_usec) }, \
-         { "force_x", NULL, MAVLINK_TYPE_INT16_T, 0, 4, offsetof(mavlink_skye_controller_output_t, force_x) }, \
-         { "force_y", NULL, MAVLINK_TYPE_INT16_T, 0, 6, offsetof(mavlink_skye_controller_output_t, force_y) }, \
-         { "force_z", NULL, MAVLINK_TYPE_INT16_T, 0, 8, offsetof(mavlink_skye_controller_output_t, force_z) }, \
-         { "moment_x", NULL, MAVLINK_TYPE_INT16_T, 0, 10, offsetof(mavlink_skye_controller_output_t, moment_x) }, \
-         { "moment_y", NULL, MAVLINK_TYPE_INT16_T, 0, 12, offsetof(mavlink_skye_controller_output_t, moment_y) }, \
-         { "moment_z", NULL, MAVLINK_TYPE_INT16_T, 0, 14, offsetof(mavlink_skye_controller_output_t, moment_z) }, \
+         { "force_x", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_skye_controller_output_t, force_x) }, \
+         { "force_y", NULL, MAVLINK_TYPE_FLOAT, 0, 8, offsetof(mavlink_skye_controller_output_t, force_y) }, \
+         { "force_z", NULL, MAVLINK_TYPE_FLOAT, 0, 12, offsetof(mavlink_skye_controller_output_t, force_z) }, \
+         { "moment_x", NULL, MAVLINK_TYPE_FLOAT, 0, 16, offsetof(mavlink_skye_controller_output_t, moment_x) }, \
+         { "moment_y", NULL, MAVLINK_TYPE_FLOAT, 0, 20, offsetof(mavlink_skye_controller_output_t, moment_y) }, \
+         { "moment_z", NULL, MAVLINK_TYPE_FLOAT, 0, 24, offsetof(mavlink_skye_controller_output_t, moment_z) }, \
          } \
 }
 
@@ -39,28 +39,28 @@ typedef struct __mavlink_skye_controller_output_t
  * @param msg The MAVLink message to compress the data into
  *
  * @param time_usec Timestamp (since UNIX epoch or microseconds since system boot)
- * @param force_x Signal given from controller to actuation chain, 0.01 Newton
- * @param force_y Signal given from controller to actuation chain, 0.01 Newton
- * @param force_z Signal given from controller to actuation chain, 0.01 Newton
- * @param moment_x Signal given from controller to actuation chain, 0.01 Newtonmeter
- * @param moment_y Signal given from controller to actuation chain, 0.01 Newtonmeter
- * @param moment_z Signal given from controller to actuation chain, 0.01 Newtonmeter
+ * @param force_x Signal given from controller to actuation chain, 1 Newton
+ * @param force_y Signal given from controller to actuation chain, 1 Newton
+ * @param force_z Signal given from controller to actuation chain, 1 Newton
+ * @param moment_x Signal given from controller to actuation chain, 1 Newtonmeter
+ * @param moment_y Signal given from controller to actuation chain, 1 Newtonmeter
+ * @param moment_z Signal given from controller to actuation chain, 1 Newtonmeter
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_skye_controller_output_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-						       uint32_t time_usec, int16_t force_x, int16_t force_y, int16_t force_z, int16_t moment_x, int16_t moment_y, int16_t moment_z)
+						       uint32_t time_usec, float force_x, float force_y, float force_z, float moment_x, float moment_y, float moment_z)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	char buf[16];
+	char buf[28];
 	_mav_put_uint32_t(buf, 0, time_usec);
-	_mav_put_int16_t(buf, 4, force_x);
-	_mav_put_int16_t(buf, 6, force_y);
-	_mav_put_int16_t(buf, 8, force_z);
-	_mav_put_int16_t(buf, 10, moment_x);
-	_mav_put_int16_t(buf, 12, moment_y);
-	_mav_put_int16_t(buf, 14, moment_z);
+	_mav_put_float(buf, 4, force_x);
+	_mav_put_float(buf, 8, force_y);
+	_mav_put_float(buf, 12, force_z);
+	_mav_put_float(buf, 16, moment_x);
+	_mav_put_float(buf, 20, moment_y);
+	_mav_put_float(buf, 24, moment_z);
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 16);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 28);
 #else
 	mavlink_skye_controller_output_t packet;
 	packet.time_usec = time_usec;
@@ -71,11 +71,11 @@ static inline uint16_t mavlink_msg_skye_controller_output_pack(uint8_t system_id
 	packet.moment_y = moment_y;
 	packet.moment_z = moment_z;
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 16);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 28);
 #endif
 
 	msg->msgid = MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT;
-	return mavlink_finalize_message(msg, system_id, component_id, 16, 28);
+	return mavlink_finalize_message(msg, system_id, component_id, 28, 103);
 }
 
 /**
@@ -85,29 +85,29 @@ static inline uint16_t mavlink_msg_skye_controller_output_pack(uint8_t system_id
  * @param chan The MAVLink channel this message was sent over
  * @param msg The MAVLink message to compress the data into
  * @param time_usec Timestamp (since UNIX epoch or microseconds since system boot)
- * @param force_x Signal given from controller to actuation chain, 0.01 Newton
- * @param force_y Signal given from controller to actuation chain, 0.01 Newton
- * @param force_z Signal given from controller to actuation chain, 0.01 Newton
- * @param moment_x Signal given from controller to actuation chain, 0.01 Newtonmeter
- * @param moment_y Signal given from controller to actuation chain, 0.01 Newtonmeter
- * @param moment_z Signal given from controller to actuation chain, 0.01 Newtonmeter
+ * @param force_x Signal given from controller to actuation chain, 1 Newton
+ * @param force_y Signal given from controller to actuation chain, 1 Newton
+ * @param force_z Signal given from controller to actuation chain, 1 Newton
+ * @param moment_x Signal given from controller to actuation chain, 1 Newtonmeter
+ * @param moment_y Signal given from controller to actuation chain, 1 Newtonmeter
+ * @param moment_z Signal given from controller to actuation chain, 1 Newtonmeter
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_skye_controller_output_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
 							   mavlink_message_t* msg,
-						           uint32_t time_usec,int16_t force_x,int16_t force_y,int16_t force_z,int16_t moment_x,int16_t moment_y,int16_t moment_z)
+						           uint32_t time_usec,float force_x,float force_y,float force_z,float moment_x,float moment_y,float moment_z)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	char buf[16];
+	char buf[28];
 	_mav_put_uint32_t(buf, 0, time_usec);
-	_mav_put_int16_t(buf, 4, force_x);
-	_mav_put_int16_t(buf, 6, force_y);
-	_mav_put_int16_t(buf, 8, force_z);
-	_mav_put_int16_t(buf, 10, moment_x);
-	_mav_put_int16_t(buf, 12, moment_y);
-	_mav_put_int16_t(buf, 14, moment_z);
+	_mav_put_float(buf, 4, force_x);
+	_mav_put_float(buf, 8, force_y);
+	_mav_put_float(buf, 12, force_z);
+	_mav_put_float(buf, 16, moment_x);
+	_mav_put_float(buf, 20, moment_y);
+	_mav_put_float(buf, 24, moment_z);
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 16);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 28);
 #else
 	mavlink_skye_controller_output_t packet;
 	packet.time_usec = time_usec;
@@ -118,11 +118,11 @@ static inline uint16_t mavlink_msg_skye_controller_output_pack_chan(uint8_t syst
 	packet.moment_y = moment_y;
 	packet.moment_z = moment_z;
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 16);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 28);
 #endif
 
 	msg->msgid = MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT;
-	return mavlink_finalize_message_chan(msg, system_id, component_id, chan, 16, 28);
+	return mavlink_finalize_message_chan(msg, system_id, component_id, chan, 28, 103);
 }
 
 /**
@@ -143,28 +143,28 @@ static inline uint16_t mavlink_msg_skye_controller_output_encode(uint8_t system_
  * @param chan MAVLink channel to send the message
  *
  * @param time_usec Timestamp (since UNIX epoch or microseconds since system boot)
- * @param force_x Signal given from controller to actuation chain, 0.01 Newton
- * @param force_y Signal given from controller to actuation chain, 0.01 Newton
- * @param force_z Signal given from controller to actuation chain, 0.01 Newton
- * @param moment_x Signal given from controller to actuation chain, 0.01 Newtonmeter
- * @param moment_y Signal given from controller to actuation chain, 0.01 Newtonmeter
- * @param moment_z Signal given from controller to actuation chain, 0.01 Newtonmeter
+ * @param force_x Signal given from controller to actuation chain, 1 Newton
+ * @param force_y Signal given from controller to actuation chain, 1 Newton
+ * @param force_z Signal given from controller to actuation chain, 1 Newton
+ * @param moment_x Signal given from controller to actuation chain, 1 Newtonmeter
+ * @param moment_y Signal given from controller to actuation chain, 1 Newtonmeter
+ * @param moment_z Signal given from controller to actuation chain, 1 Newtonmeter
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_skye_controller_output_send(mavlink_channel_t chan, uint32_t time_usec, int16_t force_x, int16_t force_y, int16_t force_z, int16_t moment_x, int16_t moment_y, int16_t moment_z)
+static inline void mavlink_msg_skye_controller_output_send(mavlink_channel_t chan, uint32_t time_usec, float force_x, float force_y, float force_z, float moment_x, float moment_y, float moment_z)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	char buf[16];
+	char buf[28];
 	_mav_put_uint32_t(buf, 0, time_usec);
-	_mav_put_int16_t(buf, 4, force_x);
-	_mav_put_int16_t(buf, 6, force_y);
-	_mav_put_int16_t(buf, 8, force_z);
-	_mav_put_int16_t(buf, 10, moment_x);
-	_mav_put_int16_t(buf, 12, moment_y);
-	_mav_put_int16_t(buf, 14, moment_z);
+	_mav_put_float(buf, 4, force_x);
+	_mav_put_float(buf, 8, force_y);
+	_mav_put_float(buf, 12, force_z);
+	_mav_put_float(buf, 16, moment_x);
+	_mav_put_float(buf, 20, moment_y);
+	_mav_put_float(buf, 24, moment_z);
 
-	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT, buf, 16, 28);
+	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT, buf, 28, 103);
 #else
 	mavlink_skye_controller_output_t packet;
 	packet.time_usec = time_usec;
@@ -175,7 +175,7 @@ static inline void mavlink_msg_skye_controller_output_send(mavlink_channel_t cha
 	packet.moment_y = moment_y;
 	packet.moment_z = moment_z;
 
-	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT, (const char *)&packet, 16, 28);
+	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_SKYE_CONTROLLER_OUTPUT, (const char *)&packet, 28, 103);
 #endif
 }
 
@@ -197,61 +197,61 @@ static inline uint32_t mavlink_msg_skye_controller_output_get_time_usec(const ma
 /**
  * @brief Get field force_x from skye_controller_output message
  *
- * @return Signal given from controller to actuation chain, 0.01 Newton
+ * @return Signal given from controller to actuation chain, 1 Newton
  */
-static inline int16_t mavlink_msg_skye_controller_output_get_force_x(const mavlink_message_t* msg)
+static inline float mavlink_msg_skye_controller_output_get_force_x(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_int16_t(msg,  4);
+	return _MAV_RETURN_float(msg,  4);
 }
 
 /**
  * @brief Get field force_y from skye_controller_output message
  *
- * @return Signal given from controller to actuation chain, 0.01 Newton
+ * @return Signal given from controller to actuation chain, 1 Newton
  */
-static inline int16_t mavlink_msg_skye_controller_output_get_force_y(const mavlink_message_t* msg)
+static inline float mavlink_msg_skye_controller_output_get_force_y(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_int16_t(msg,  6);
+	return _MAV_RETURN_float(msg,  8);
 }
 
 /**
  * @brief Get field force_z from skye_controller_output message
  *
- * @return Signal given from controller to actuation chain, 0.01 Newton
+ * @return Signal given from controller to actuation chain, 1 Newton
  */
-static inline int16_t mavlink_msg_skye_controller_output_get_force_z(const mavlink_message_t* msg)
+static inline float mavlink_msg_skye_controller_output_get_force_z(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_int16_t(msg,  8);
+	return _MAV_RETURN_float(msg,  12);
 }
 
 /**
  * @brief Get field moment_x from skye_controller_output message
  *
- * @return Signal given from controller to actuation chain, 0.01 Newtonmeter
+ * @return Signal given from controller to actuation chain, 1 Newtonmeter
  */
-static inline int16_t mavlink_msg_skye_controller_output_get_moment_x(const mavlink_message_t* msg)
+static inline float mavlink_msg_skye_controller_output_get_moment_x(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_int16_t(msg,  10);
+	return _MAV_RETURN_float(msg,  16);
 }
 
 /**
  * @brief Get field moment_y from skye_controller_output message
  *
- * @return Signal given from controller to actuation chain, 0.01 Newtonmeter
+ * @return Signal given from controller to actuation chain, 1 Newtonmeter
  */
-static inline int16_t mavlink_msg_skye_controller_output_get_moment_y(const mavlink_message_t* msg)
+static inline float mavlink_msg_skye_controller_output_get_moment_y(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_int16_t(msg,  12);
+	return _MAV_RETURN_float(msg,  20);
 }
 
 /**
  * @brief Get field moment_z from skye_controller_output message
  *
- * @return Signal given from controller to actuation chain, 0.01 Newtonmeter
+ * @return Signal given from controller to actuation chain, 1 Newtonmeter
  */
-static inline int16_t mavlink_msg_skye_controller_output_get_moment_z(const mavlink_message_t* msg)
+static inline float mavlink_msg_skye_controller_output_get_moment_z(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_int16_t(msg,  14);
+	return _MAV_RETURN_float(msg,  24);
 }
 
 /**
@@ -271,6 +271,6 @@ static inline void mavlink_msg_skye_controller_output_decode(const mavlink_messa
 	skye_controller_output->moment_y = mavlink_msg_skye_controller_output_get_moment_y(msg);
 	skye_controller_output->moment_z = mavlink_msg_skye_controller_output_get_moment_z(msg);
 #else
-	memcpy(skye_controller_output, _MAV_PAYLOAD(msg), 16);
+	memcpy(skye_controller_output, _MAV_PAYLOAD(msg), 28);
 #endif
 }
